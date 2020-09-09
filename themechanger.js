fc01709d5e8d1cb528d8c23a9c0de1a8ef987873
Shahin Mohseni <shahin.mohseni@learnet.se>
2020-09-09 09:00:17 +0200

    logo invert fixed

--- themechanger.js
+++ themechanger.js
@@ -14,13 +14,23 @@ if (theme) {
 }
 
 if (theme === 'dark') {
-    logo.classList.add('rev');
+    body.classList.replace('light', 'dark');
+    logo.classList.replace('inv', 'rev');
+    button.classList.replace('btn-light', 'btn-dark');
     darkButton.classList.replace('theme-changer-light', 'theme-changer-dark');
     lightButton.classList.replace('theme-changer-light', 'theme-changer-dark');
+
+    localStorage.setItem('theme', 'dark');
+    console.log(`Theme set to dark.`);    
 } else {
-    logo.classList.add('inv');
-    darkButton.classList.replace('theme-changer-dark', 'theme-changer-light');
+    body.classList.replace('dark', 'light');
+    logo.classList.replace('rev', 'inv');
+    button.classList.replace('btn-dark', 'btn-light');
     lightButton.classList.replace('theme-changer-dark', 'theme-changer-light');
+    darkButton.classList.replace('theme-changer-dark', 'theme-changer-light');
+
+    localStorage.setItem('theme', 'light');
+    console.log(`Theme set to light.`);  
 }
 
 darkButton.onclick = () => {
