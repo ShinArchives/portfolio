ae6a81aa630e7bd64fb854e889a2fd60f7ee0ec9
Shahin Mohseni <shahin.mohseni@learnet.se>
2020-09-09 09:06:23 +0200

    testing to fix theme on startup

--- themechanger.js
+++ themechanger.js
@@ -14,20 +14,20 @@ if (theme) {
 }
 
 if (theme === 'dark') {
-    body.classList.replace('light', 'dark');
-    logo.classList.replace('inv', 'rev');
-    button.classList.replace('btn-light', 'btn-dark');
-    darkButton.classList.replace('theme-changer-light', 'theme-changer-dark');
-    lightButton.classList.replace('theme-changer-light', 'theme-changer-dark');
+    body.classList.add('dark');
+    logo.classList.add('rev');
+    button.classList.add('btn-dark');
+    darkButton.classList.add('theme-changer-dark');
+    lightButton.classList.add('theme-changer-dark');
 
     localStorage.setItem('theme', 'dark');
     console.log(`Theme set to dark.`);    
 } else {
-    body.classList.replace('dark', 'light');
-    logo.classList.replace('rev', 'inv');
-    button.classList.replace('btn-dark', 'btn-light');
-    lightButton.classList.replace('theme-changer-dark', 'theme-changer-light');
-    darkButton.classList.replace('theme-changer-dark', 'theme-changer-light');
+    body.classList.add('light');
+    logo.classList.add('inv');
+    button.classList.add('btn-light');
+    lightButton.classList.add('theme-changer-light');
+    darkButton.classList.add('theme-changer-light');
 
     localStorage.setItem('theme', 'light');
     console.log(`Theme set to light.`);  
