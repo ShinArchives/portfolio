914d99926bde706efbcb41e0c33f450fc2f07848
Shahin Mohseni <shahin.mohseni@learnet.se>
2020-09-22 10:34:45 +0200

    theme fixed - tack kishi

--- themechanger.js
+++ themechanger.js
@@ -1,5 +1,4 @@
 const darkButton = document.getElementById('dark');
-const lightButton = document.getElementById('light');
 
 const body = document.body;
 
@@ -9,48 +8,68 @@ const theme = localStorage.getItem('theme');
 const button = document.getElementById('backToTop');
 const logo = document.getElementById('logo');
 
+let isDarkTrue;
+
 if (theme) {
     body.classList.add(theme);
+}
+
+const toDark = () => {
+    body.classList.replace('light', 'dark');
+    logo.classList.replace('inv', 'rev');
+    button.classList.replace('btn-light', 'btn-dark');
+    darkButton.classList.replace('theme-changer-light', 'theme-changer-dark');
+
+    localStorage.setItem('theme', 'dark');
+    console.log(`Theme set to dark.`);
+
+    darkButton.textContent = 'Light Theme ☀️';
+    isDarkTrue = true;
+    return;
+}
+
+const toLight = () => {
+    body.classList.replace('dark', 'light');
+    logo.classList.replace('rev', 'inv');
+    button.classList.replace('btn-dark', 'btn-light');
+    darkButton.classList.replace('theme-changer-dark', 'theme-changer-light');
+
+    localStorage.setItem('theme', 'light');
+    console.log(`Theme set to light.`);
+
+    darkButton.textContent = 'Dark Theme 🌙';
+    isDarkTrue = false;
+    return;
 }
 
 if (theme === 'light') {
     body.classList.replace('dark', 'light');
     logo.classList.add('inv');
     button.classList.add('btn-light');
-    lightButton.classList.add('theme-changer-light');
     darkButton.classList.add('theme-changer-light');
 
     localStorage.setItem('theme', 'light');
     console.log(`Theme set to light.`);
+
+    darkButton.textContent = 'Dark Theme 🌙';
+    isDarkTrue = false;
 } else {
     body.classList.replace('light', 'dark');
     logo.classList.add('rev');
     button.classList.add('btn-dark');
     darkButton.classList.add('theme-changer-dark');
-    lightButton.classList.add('theme-changer-dark');
 
     localStorage.setItem('theme', 'dark');
     console.log(`Theme set to dark.`);
+
+    darkButton.textContent = 'Light Theme ☀️';
+    isDarkTrue = true;
 }
 
 darkButton.onclick = () => {
-    body.classList.replace('light', 'dark');
-    logo.classList.replace('inv', 'rev');
-    button.classList.replace('btn-light', 'btn-dark');
-    darkButton.classList.replace('theme-changer-light', 'theme-changer-dark');
-    lightButton.classList.replace('theme-changer-light', 'theme-changer-dark');
-
-    localStorage.setItem('theme', 'dark');
-    console.log(`Theme set to dark.`);
-};
-
-lightButton.onclick = () => {
-    body.classList.replace('dark', 'light');
-    logo.classList.replace('rev', 'inv');
-    button.classList.replace('btn-dark', 'btn-light');
-    lightButton.classList.replace('theme-changer-dark', 'theme-changer-light');
-    darkButton.classList.replace('theme-changer-dark', 'theme-changer-light');
-
-    localStorage.setItem('theme', 'light');
-    console.log(`Theme set to light.`);
+    if (!isDarkTrue) {
+        toDark();
+    } else {
+        toLight();
+    }
 };
